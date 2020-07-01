28fe832543878de8d44a8a601f9f169e2050fb1b
Ashley Hines <ashhines95@gmail.com>
2020-07-01 17:51:36 +1000

    Modified some frontend

--- public/javascripts/clientside.js
+++ public/javascripts/clientside.js
@@ -6,16 +6,17 @@ function updateValues() {
         //console.log(res);
         var primeResponse = JSON.parse(res);
         //console.log(primeResponse);
-        document.getElementById("title").innerHTML = "Cisco Prime All Good!!";
+        document.getElementById("title").innerHTML = "<h2>Cisco Prime Monitoring Dashboard</h2>";
         document.getElementById("reachable").innerHTML = "Reachable devices = " + primeResponse.reachable['@count']; 
-        document.getElementById("unreachable").innerHTML = "unreachable devices = " + primeResponse.unreachable['@count']; 
+        document.getElementById("unreachable").innerHTML = "unreachable devices = " + primeResponse.unreachable['@count'] + "<br>Unreachable Devices:<ul>";
         if (primeResponse.unreachableDevices) {
             primeResponse.unreachableDevices.forEach((device) => {
-                //document.getElementById("unreachable").innerHTML +=
+                document.getElementById("unreachable").innerHTML += "<li>" + device.deviceName + "</li>";
                 console.log(device)
             });
             
         }
+        document.getElementById("unreachable").innerHTML += "</ul>";
     })
 }
 
